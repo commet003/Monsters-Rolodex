9421f06ce37220e3b2494d174d49253aaaf045e4
Corie Rhodes <carhodes@deakin.edu.au>
2019-10-31 17:35:06 +1100

    Updates

---
+++ precache-manifest.988fe29609dbc5ddba1748fc7b57af9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb688f19db01958f51e0c3b35f41404",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "1d254ab5381b2348b8d4",
+    "url": "/Monsters-Rolodex/static/css/main.6bb7eb7f.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/Monsters-Rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "1d254ab5381b2348b8d4",
+    "url": "/Monsters-Rolodex/static/js/main.7ba11440.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
